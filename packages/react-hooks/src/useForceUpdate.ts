26dcaaf2f41008c7c7701157cf46e199eda843d9
mo36924 <mo36924@users.noreply.github.com>
2021-03-06 16:20:32 +0900

    ✨

--- packages/react-hooks/src/useForceUpdate.ts
+++
@@ -1,5 +0,0 @@
-import { useReducer } from "react";
-
-const reducer = (x: number) => x + 1;
-
-export const useForceUpdate = (): (() => void) => useReducer(reducer, 0)[1];
--- packages/route-generator/src/index.ts
+++ packages/route-generator/src/index.ts
@@ -23,6 +23,7 @@ const defaultOptions: Required<Options> = {
 };
 
 const defaultTemplate = `
+import changestate from "@mo36924/changestate"
 import lazy from "@mo36924/preact-lazy";
 import { ComponentType, createContext } from "preact";
 import { useState, useEffect } from "preact/hooks";
@@ -70,76 +71,6 @@ export const match = (url: string | URL): RouteContextValue => {
 
   return { url, route, props };
 };
-
-const changestate = "changestate";
-
-declare global {
-  interface Window {
-    onchangestate?: null | ((this: Window, ev: Event) => any);
-  }
-}
-
-if (typeof window !== "undefined" && window.onchangestate === undefined) {
-  window.onchangestate = null;
-
-  const createEvent =
-    typeof Event === "function"
-      ? (type: string) => new Event(type)
-      : (type: string) => {
-          const event = document.createEvent("Event");
-          event.initEvent(type, false, false);
-          return event;
-        };
-
-  const events = ["pushState", "replaceState"] as const;
-  const origin = location.origin + "/";
-
-  events.forEach((method) => {
-    const original = history[method].bind(history);
-    const type = method.toLowerCase();
-
-    history[method] = (...args) => {
-      original(...args);
-      dispatchEvent(createEvent(type));
-    };
-  });
-
-  [...events, "popstate"].forEach((type) => {
-    addEventListener(type.toLowerCase(), () => {
-      const event = createEvent(changestate);
-      dispatchEvent(event);
-
-      if (typeof window.onchangestate === "function") {
-        window.onchangestate(event);
-      }
-    });
-  });
-
-  addEventListener("click", (e) => {
-    if (e.ctrlKey || e.metaKey || e.altKey || e.shiftKey || e.button) {
-      return;
-    }
-
-    let t: any = e.target;
-
-    do {
-      if (\`\${t.nodeName}\`.toLowerCase() === "a") {
-        if (t.hasAttribute("download") || t.getAttribute("rel") === "external") {
-          return;
-        }
-
-        const url = t.href;
-
-        if (url.startsWith(origin)) {
-          e.preventDefault();
-          history.pushState(null, "", url);
-        }
-
-        return;
-      }
-    } while ((t = t.parentNode));
-  });
-}
 
 export default (props: { url: string | URL }) => {
   const [state, setState] = useState(match(props.url));
