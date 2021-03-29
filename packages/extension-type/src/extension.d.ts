66d6594013a4cc4e682710816387f9b1aeb01b87
mo36924 <mo36924@users.noreply.github.com>
2021-03-29 22:21:07 +0900

    ✨

---
+++ packages/extension-type/src/extension.d.ts
@@ -0,0 +1,8 @@
+declare module "*.gql" {
+  import type { DocumentNode } from "graphql";
+  export default {} as DocumentNode;
+}
+declare module "*.graphql" {
+  import type { DocumentNode } from "graphql";
+  export default {} as DocumentNode;
+}
