5765554e317239463ed532f209660b48bc615b1d
mo36924 <mo36924@users.noreply.github.com>
2021-03-31 13:05:25 +0900

    🐛

--- packages/graphql-react/src/cache.client.ts
+++ packages/graphql-react/src/cache.client.ts
@@ -2,7 +2,4 @@ import { parse } from "@mo36924/graphql-json";
 
 const graphql = document.getElementById("graphql");
 
-export const cache: { [url: string]: any } = Object.assign(
-  Object.create(null),
-  graphql && JSON.parse(graphql.innerHTML, parse),
-);
+export const cache: { [url: string]: any } = Object.assign(Object.create(null), graphql && parse(graphql.innerHTML));
