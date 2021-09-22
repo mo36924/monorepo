2e629a70c46300f44617cf490a59dc155ba3030f
mo36924 <mo36924@users.noreply.github.com>
2021-09-23 06:58:15 +0900

    ♻️

--- packages/graphql-codegen-typescript/src/index.ts
+++ packages/graphql-codegen-typescript/src/index.ts
@@ -1,9 +1,13 @@
 import type { PluginFunction } from "@graphql-codegen/plugin-helpers";
 import { plugin as typescript, TypeScriptPluginConfig as _TypeScriptPluginConfig } from "@graphql-codegen/typescript";
 import "@mo36924/graphql-codegen-prettier";
-import { stripIgnoredCharacters } from "graphql";
+import { Source, stripIgnoredCharacters } from "graphql";
+// eslint-disable-next-line import/order
+import { relative, sep } from "path";
 
 export type TypeScriptPluginConfig = _TypeScriptPluginConfig & { minify?: boolean };
+
+const cwd = process.cwd() + sep;
 
 export const plugin: PluginFunction<TypeScriptPluginConfig> = async (schema, documents, config, info) => {
   let output = await typescript(schema, documents, config, info);
@@ -13,7 +17,7 @@ export const plugin: PluginFunction<TypeScriptPluginConfig> = async (schema, doc
     return output;
   }
 
-  let { body, name } = schema.astNode?.loc?.source ?? {};
+  let { body, name }: Partial<Source> = schema.astNode?.loc?.source ?? schema.extensions?.sources?.[0] ?? {};
 
   if (body === undefined) {
     return output;
@@ -23,12 +27,18 @@ export const plugin: PluginFunction<TypeScriptPluginConfig> = async (schema, doc
     body = stripIgnoredCharacters(body);
   }
 
+  if (name?.startsWith(cwd)) {
+    name = relative(cwd, name);
+  } else {
+    name = undefined;
+  }
+
   return {
     content: output.content,
     prepend: ['import { Source } from "graphql";\n', ...(output.prepend ?? [])],
     append: [
       ...(output.append ?? []),
-      `\n\nexport default new Source(${JSON.stringify(body)}, ${JSON.stringify(name)});\n\n`,
+      `\n\nexport default new Source(${JSON.stringify(body)}${name ? `, ${JSON.stringify(name)}` : ""});\n\n`,
     ],
   };
 };
